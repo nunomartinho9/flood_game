301fce3480c44725eceed737236955d7a7449f76
Nuno Martinho <47896130+nunomartinho9@users.noreply.github.com>
2024-08-17 19:26:41 +0100

    [Day 3] -Entity Structure, -SpriteID, -Generic Sprite Entity Render

--- entry_floodgame.c
+++ entry_floodgame.c
@@ -1,9 +1,104 @@
+typedef struct Sprite
+{
+	Gfx_Image *image;
+	Vector2 size;
 
-int entry(int argc, char **argv) {
-	
-	window.title = STR("FLOOD GAME SURVIVAL");
+} Sprite;
+typedef enum SpriteID
+{
+	SPRITE_nil,
+	SPRITE_player,
+	SPRITE_tree0,
+	SPRITE_tree1,
+	SPRITE_rock0,
+	SPRITE_MAX,
+} SpriteID;
+// nuno: maybe we make this an X macro?? https://chatgpt.com/share/260222eb-2738-4d1e-8b1d-4973a097814d
+Sprite sprites[SPRITE_MAX];
+
+Sprite* get_sprite(SpriteID id) {
+	if (id >= 0 && id < SPRITE_MAX) {
+		return &sprites[id];
+	}
+
+	return &sprites[0];
+}
+typedef enum EntityArchetype
+{
+	arch_nil = 0,
+	arch_rock = 1,
+	arch_tree = 2,
+	arch_player = 3,
+} EntityArchetype;
+
+typedef struct Entity
+{
+	Vector2 pos;
+	bool is_valid;
+	EntityArchetype arch;
+
+	bool render_sprite;
+	SpriteID sprite_id;
+
+} Entity;
+#define MAX_ENTITY_COUNT 1024
+typedef struct World
+{
+	Entity entities[MAX_ENTITY_COUNT];
+
+} World;
+World *world = 0;
+
+Entity *entity_create()
+{
+	Entity *entity_found = 0;
+	for (int i = 0; i < MAX_ENTITY_COUNT; i++)
+	{
+		Entity *existing_entity = &world->entities[i];
+		if (!existing_entity->is_valid)
+		{
+			entity_found = existing_entity;
+			break;
+		}
+	}
+	assert(entity_found, "No more free entities!");
+	entity_found->is_valid = true;
+	return (entity_found);
+}
+
+void entity_destroy(Entity *entity)
+{
+	memset(entity, 0, sizeof(Entity));
+}
+
+void setup_rock(Entity *entity)
+{
+	entity->arch = arch_rock;
+	entity->sprite_id = SPRITE_rock0;
+	//....
+}
+
+void setup_tree(Entity *entity)
+{
+	entity->arch = arch_tree;
+	entity->sprite_id = SPRITE_tree0;
+	//....
+}
+
+void setup_player(Entity *entity)
+{
+	entity->arch = arch_player;
+	entity->sprite_id = SPRITE_player;
+	//....
+}
+
+
+int entry(int argc, char **argv)
+{
+
+	window.title = STR("DANTES INF GAME SURVIVAL");
 	window.scaled_width = 1280; // We need to set the scaled size if we want to handle system scaling (DPI)
-	window.scaled_height = 720; 
+	window.scaled_height = 720;
 	window.x = 200;
 	window.y = 90;
 	window.clear_color = hex_to_rgba(0x0b0b0bff);
@@ -12,72 +107,115 @@ int entry(int argc, char **argv) {
 	const float64 fps_limit = 69000;
 	const float64 min_frametime = 1.0 / fps_limit;
 	const float speed = 50.0;
-	Gfx_Image *player_image = load_image_from_disk(fixed_string("player.png"), get_heap_allocator());
-	assert(player_image, "player.png failed to load.");
 
-	Vector2 player_pos = v2(0, 0);
+	world = alloc(get_heap_allocator(), sizeof(World));
+	memset(world, 0, sizeof(World));
 
-	
-	//this while is the game running (every single frame)
-	while (!window.should_close) {
+	sprites[SPRITE_player] = (Sprite){.image = load_image_from_disk(fixed_string("player.png"), get_heap_allocator()), .size = v2(14, 17)};
+	sprites[SPRITE_tree0] = (Sprite){.image = load_image_from_disk(fixed_string("tree0.png"), get_heap_allocator()), .size = v2(11, 13)};
+	sprites[SPRITE_rock0] = (Sprite){.image = load_image_from_disk(fixed_string("rock0.png"), get_heap_allocator()), .size = v2(9, 5)};
+
+	Entity *player_en = entity_create();
+	setup_player(player_en);
+	for (int i = 0; i < 10; i++)
+	{
+		Entity *en = entity_create();
+		setup_rock(en);
+		en->pos = v2(get_random_float32_in_range(-100, 100), get_random_float32_in_range(-100, 100));
+	}
+
+	for (int i = 0; i < 4; i++)
+	{
+		Entity *en = entity_create();
+		setup_tree(en);
+		en->pos = v2(get_random_float32_in_range(-100, 100), get_random_float32_in_range(-100, 100));
+	}
+
+	// this while is the game running (every single frame)
+	while (!window.should_close)
+	{
+		reset_temporary_storage();
 
 		draw_frame.projection = m4_make_orthographic_projection(window.width * -0.5, window.width * 0.5, window.height * -0.5, window.height * 0.5, -1, 10);
-		float zoom = 10;
-		draw_frame.view = m4_make_scale(v3(1.0/zoom, 1.0/zoom, 1.0));
+		float zoom = 5.3;
+		draw_frame.view = m4_make_scale(v3(1.0 / zoom, 1.0 / zoom, 1.0));
 
 		float64 now = os_get_current_time_in_seconds();
-		if ((int)now != (int)last_time) log("%.2f FPS\n%.2fms", 1.0/(now-last_time), (now-last_time)*1000); //fps
+		if ((int)now != (int)last_time)
+			log("%.2f FPS\n%.2fms", 1.0 / (now - last_time), (now - last_time) * 1000); // fps
 		float64 delta = now - last_time;
-		if (delta < min_frametime) {
-			os_high_precision_sleep((min_frametime-delta)*1000.0);
+		if (delta < min_frametime)
+		{
+			os_high_precision_sleep((min_frametime - delta) * 1000.0);
 			now = os_get_current_time_in_seconds();
 			delta = now - last_time;
 		}
 		last_time = now;
-		
-		reset_temporary_storage();
-		
-		if (is_key_just_released(KEY_ESCAPE)) {
+
+		// :render images for each arch type
+		for (int i = 0; i < MAX_ENTITY_COUNT; i++)
+		{
+			Entity *en = &world->entities[i];
+
+			if (en->is_valid)
+			{
+				switch (en->arch)
+				{
+
+				default:
+				{
+					Sprite *sprite = get_sprite(en->sprite_id);
+					Matrix4 xform = m4_scalar(1.0);
+					xform = m4_translate(xform, v3(en->pos.x, en->pos.y, 0));
+					xform = m4_translate(xform, v3(sprite->size.x * -0.5, 0.0, 0));
+					draw_image_xform(sprite->image, xform, sprite->size, COLOR_WHITE);
+				}
+				break;
+				}
+			}
+		}
+
+		if (is_key_just_released(KEY_ESCAPE))
+		{
 			window.should_close = true;
 		}
 
-		//getting the input of the player
+		// getting the input of the player
 		Vector2 input_axis = v2(0, 0);
-		if (is_key_down('A')) {
+		if (is_key_down('A'))
+		{
 			input_axis.x -= 1.0;
 		}
-		if (is_key_down('D')) {
+		if (is_key_down('D'))
+		{
 			input_axis.x += 1.0;
 		}
-		if (is_key_down('S')) {
+		if (is_key_down('S'))
+		{
 			input_axis.y -= 1.0;
 		}
-		if (is_key_down('W')) {
+		if (is_key_down('W'))
+		{
 			input_axis.y += 1.0;
 		}
 		input_axis = v2_normalize(input_axis);
-		//adding to the vector2 (player_pos) the player pos + the input (direction) times the speed
-		player_pos = v2_add(player_pos, v2_mulf(input_axis, speed * delta)); //0.0002f is a constant basically the speed of the player movement
-		/* 
+		// adding to the vector2 (player_pos) the player pos + the input (direction) times the speed
+		player_en->pos = v2_add(player_en->pos, v2_mulf(input_axis, speed * delta)); // 0.0002f is a constant basically the speed of the player movement
+		/*
 		Matrix4 rect_xform = m4_scalar(1.0);
 		rect_xform         = m4_rotate_z(rect_xform, (f32)now);
 		rect_xform         = m4_translate(rect_xform, v3(-.25f, -.25f, 0));
 		draw_rect_xform(rect_xform, v2(.5f, .5f), COLOR_GREEN);
-		
+
 		draw_rect(v2(sin(now), -.8), v2(.5, .25), COLOR_RED); */
-		
-        /*float aspect = (f32)window.width/(f32)window.height;
+
+		/*float aspect = (f32)window.width/(f32)window.height;
 		float mx = (input_frame.mouse_x/(f32)window.width  * 2.0 - 1.0)*aspect;
 		float my = input_frame.mouse_y/(f32)window.height * 2.0 - 1.0; */
-		
-		//draw_line(v2(-.75, -.75), v2(mx, my), 0.005, COLOR_WHITE);
 
-		Vector2 size = v2(14, 17);
-		Matrix4 player_xform = m4_scalar(.3);
-		player_xform         = m4_translate(player_xform, v3(player_pos.x, player_pos.y, 0));
-		draw_image_xform(player_image, player_xform, size, COLOR_WHITE);
-		
-		os_update(); 
+		// draw_line(v2(-.75, -.75), v2(mx, my), 0.005, COLOR_WHITE);
+
+		os_update();
 		gfx_update();
 	}
 
